9fd3d2bd66d390b9ff413766c41fde261e4cfe8a
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-09-05 12:32:44 +0100

    Update InstallMySQLClient.sh

--- installscripts/InstallMySQLClient.sh
+++ installscripts/InstallMySQLClient.sh
@@ -49,22 +49,22 @@ then
     if ( [ "${BUILDOS}" = "ubuntu" ] )
     then
         DEBIAN_FRONTEND=noninteractive ${apt} -o DPkg::Lock::Timeout=-1 -qq -y install gnupg
-       # mysql_apt_config="`/usr/bin/wget -O- https://dev.mysql.com/downloads/repo/apt/ | /bin/grep -o mysql-apt-config.* | /usr/bin/head -1 | /bin/sed 's/deb.*/deb/g'`"
-       # /usr/bin/wget https://dev.mysql.com/get/${mysql_apt_config} 
-       # DEBIAN_FRONTEND=noninteractive /usr/bin/dpkg -i ${mysql_apt_config}
-       # /bin/rm ${mysql_apt_config}
+        mysql_apt_config="`/usr/bin/wget -O- https://dev.mysql.com/downloads/repo/apt/ | /bin/grep -o mysql-apt-config.* | /usr/bin/head -1 | /bin/sed 's/deb.*/deb/g'`"
+        /usr/bin/wget https://dev.mysql.com/get/${mysql_apt_config} 
+        DEBIAN_FRONTEND=noninteractive /usr/bin/dpkg -i ${mysql_apt_config}
+        /bin/rm ${mysql_apt_config}
         ${HOME}/installscripts/Update.sh ${BUILDOS}
-        DEBIAN_FRONTEND=noninteractive ${apt} -o DPkg::Lock::Timeout=69 -qq -y install default-mysql-client
+        DEBIAN_FRONTEND=noninteractive ${apt} -o DPkg::Lock::Timeout=69 -qq -y install mysql-client
     fi
 
     if ( [ "${BUILDOS}" = "debian" ] )
     then
         DEBIAN_FRONTEND=noninteractive ${apt} -o DPkg::Lock::Timeout=-1 -qq -y install gnupg
-      #  mysql_apt_config="`/usr/bin/wget -O- https://dev.mysql.com/downloads/repo/apt/ | /bin/grep -o mysql-apt-config.* | /usr/bin/head -1 | /bin/sed 's/deb.*/deb/g'`"
-      #  /usr/bin/wget https://dev.mysql.com/get/${mysql_apt_config} 
-      #  DEBIAN_FRONTEND=noninteractive /usr/bin/dpkg -i ${mysql_apt_config}
-      #  /bin/rm ${mysql_apt_config}
+        mysql_apt_config="`/usr/bin/wget -O- https://dev.mysql.com/downloads/repo/apt/ | /bin/grep -o mysql-apt-config.* | /usr/bin/head -1 | /bin/sed 's/deb.*/deb/g'`"
+        /usr/bin/wget https://dev.mysql.com/get/${mysql_apt_config} 
+        DEBIAN_FRONTEND=noninteractive /usr/bin/dpkg -i ${mysql_apt_config}
+        /bin/rm ${mysql_apt_config}
         ${HOME}/installscripts/Update.sh ${BUILDOS}
-        DEBIAN_FRONTEND=noninteractive ${apt} -o DPkg::Lock::Timeout=60 -qq -y install default-mysql-client
+        DEBIAN_FRONTEND=noninteractive ${apt} -o DPkg::Lock::Timeout=60 -qq -y install mysql-client
     fi
 fi
