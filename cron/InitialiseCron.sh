889f289fa7ab894c552e90af8f2baea32422dadf
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-10-26 13:35:17 +0100

    Update InitialiseCron.sh

--- cron/InitialiseCron.sh
+++ cron/InitialiseCron.sh
@@ -30,13 +30,6 @@
 /bin/echo "*/1 * * * * export HOME="${HOMEDIR}" && ${HOME}/providerscripts/utilities/MonitorForOverload.sh" >> /var/spool/cron/crontabs/root
 /bin/echo "*/1 * * * * export HOME="${HOMEDIR}" && ${HOME}/providerscripts/utilities/IsDatabaseUp.sh" >> /var/spool/cron/crontabs/root
 
-
-if ( [ "`${HOME}/providerscripts/utilities/CheckConfigValue.sh GATEWAYGUARDIAN:1`" = "1" ] )
-then
-    /bin/echo "*/1 * * * * export HOME="${HOMEDIR}" && ${HOME}/security/GatewayGuardian.sh" >> /var/spool/cron/crontabs/root
-    /bin/echo "#@monthly export HOME="${HOMEDIR}" && ${HOME}/security/GatewayGuardian.sh 'fromcronreset'" >> /var/spool/cron/crontabs/root
-fi
-
 #These scripts run every 5 minutes
 /bin/echo "*/5 * * * * export HOME="${HOMEDIR}" && ${HOME}/security/MonitorFirewall.sh" >> /var/spool/cron/crontabs/root
 
