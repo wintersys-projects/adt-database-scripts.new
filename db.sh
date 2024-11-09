51631850f777e24ecdafca4395fed40af8f74e2d
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-11-09 13:52:06 +0000

    Update db.sh

--- db.sh
+++ db.sh
@@ -173,33 +173,36 @@ kernel.panic=10" >> /etc/sysctl.conf
 /bin/echo "${0} #######################################################################################" >> ${HOME}/logs/initialbuild/BUILD_PROCESS_MONITORING.log
 #Update and upgrade the software to its latest available versions
 
->&2 /bin/echo "${0} Update.sh"
-${HOME}/installscripts/Update.sh ${BUILDOS}
+${HOME}/installscripts/InstallALL.sh 
 
->&2 /bin/echo "${0} InstallSoftwareProperties.sh"
-${HOME}/installscripts/InstallSoftwareProperties.sh ${BUILDOS}
->&2 /bin/echo "${0} InstallCurl.sh"
-${HOME}/installscripts/InstallCurl.sh ${BUILDOS}
->&2 /bin/echo "${0} InstallLibSocketSSL.sh"
-${HOME}/installscripts/InstallLibioSocketSSL.sh ${BUILDOS}
->&2 /bin/echo "${0} InstallLibnetSSLLeay.sh"
-${HOME}/installscripts/InstallLibnetSSLLeay.sh ${BUILDOS}
->&2 /bin/echo "${0} InstallSendEmail.sh"
-${HOME}/installscripts/InstallSendEmail.sh ${BUILDOS}
->&2 /bin/echo "${0} InstallSysStat.sh"
-${HOME}/installscripts/InstallSysStat.sh ${BUILDOS}
->&2 /bin/echo "${0} InstallFirewall.sh"
-${HOME}/installscripts/InstallFirewall.sh ${BUILDOS}
->&2 /bin/echo "${0} InstallRsync.sh"
-${HOME}/installscripts/InstallRsync.sh ${BUILDOS}
->&2 /bin/echo "${0} InstallJQ.sh"
-${HOME}/installscripts/InstallJQ.sh ${BUILDOS}
->&2 /bin/echo "${0} InstallCron.sh"
-${HOME}/installscripts/InstallCron.sh ${BUILDOS}
->&2 /bin/echo "${0} InstallGo.sh"
-${HOME}/installscripts/InstallGo.sh ${BUILDOS}
 
-${HOME}/installscripts/InstallMonitoringGear.sh
+#>&2 /bin/echo "${0} Update.sh"
+#${HOME}/installscripts/Update.sh ${BUILDOS}
+
+#>&2 /bin/echo "${0} InstallSoftwareProperties.sh"
+#${HOME}/installscripts/InstallSoftwareProperties.sh ${BUILDOS}
+#>&2 /bin/echo "${0} InstallCurl.sh"
+#${HOME}/installscripts/InstallCurl.sh ${BUILDOS}
+#>&2 /bin/echo "${0} InstallLibSocketSSL.sh"
+#${HOME}/installscripts/InstallLibioSocketSSL.sh ${BUILDOS}
+#>&2 /bin/echo "${0} InstallLibnetSSLLeay.sh"
+#${HOME}/installscripts/InstallLibnetSSLLeay.sh ${BUILDOS}
+#>&2 /bin/echo "${0} InstallSendEmail.sh"
+#${HOME}/installscripts/InstallSendEmail.sh ${BUILDOS}
+#>&2 /bin/echo "${0} InstallSysStat.sh"
+#${HOME}/installscripts/InstallSysStat.sh ${BUILDOS}
+#>&2 /bin/echo "${0} InstallFirewall.sh"
+#${HOME}/installscripts/InstallFirewall.sh ${BUILDOS}
+#>&2 /bin/echo "${0} InstallRsync.sh"
+#${HOME}/installscripts/InstallRsync.sh ${BUILDOS}
+#>&2 /bin/echo "${0} InstallJQ.sh"
+#${HOME}/installscripts/InstallJQ.sh ${BUILDOS}
+#>&2 /bin/echo "${0} InstallCron.sh"
+#${HOME}/installscripts/InstallCron.sh ${BUILDOS}
+#>&2 /bin/echo "${0} InstallGo.sh"
+#${HOME}/installscripts/InstallGo.sh ${BUILDOS}
+
+#${HOME}/installscripts/InstallMonitoringGear.sh
 
 /bin/echo "${0} #######################################################################################" >> ${HOME}/logs/initialbuild/BUILD_PROCESS_MONITORING.log
 >&2 /bin/echo "${0} Setting timezone"
