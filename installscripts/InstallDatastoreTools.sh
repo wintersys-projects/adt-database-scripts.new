86d1bc038cd504d854af5e52777a2add7bb72f9c
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-11-09 14:34:50 +0000

    Update InstallDatastoreTools.sh

--- installscripts/InstallDatastoreTools.sh
+++ installscripts/InstallDatastoreTools.sh
@@ -18,7 +18,7 @@
 # along with The Agile Deployment Toolkit.  If not, see <http://www.gnu.org/licenses/>.
 #######################################################################################################
 #######################################################################################################
-#set -x
+set -x
 
 BUILDOS="`${HOME}/providerscripts/utilities/ExtractConfigValue.sh 'BUILDOS'`"
 
