3e03ecbf4efd1ae24b2d2a9572d5fca1a6a1cd87
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-11-14 17:43:08 +0000

    Delete installscripts/InstallMySQLServer.sh

--- installscripts/InstallMySQLServer.sh
+++
@@ -1,9 +0,0 @@
-apt update
-apt install -y curl
-curl -O https://repo.percona.com/apt/percona-release_latest.generic_all.deb
-apt install -y gnupg2 lsb-release ./percona-release_latest.generic_all.deb
-apt update
-percona-release setup ps80
-percona-release enable ps-80 release
-apt update
-apt install -y -qq percona-server-server
