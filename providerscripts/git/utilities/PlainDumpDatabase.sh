778ef000ae06ea56c5f22b5fc158b505cd91a8e1
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-11-15 01:31:45 +0000

    Update PlainDumpDatabase.sh

--- providerscripts/git/utilities/PlainDumpDatabase.sh
+++ providerscripts/git/utilities/PlainDumpDatabase.sh
@@ -40,12 +40,12 @@ if ( [ "`${HOME}/providerscripts/utilities/CheckConfigValue.sh DATABASEINSTALLAT
 then    
     /bin/echo "SET SESSION sql_require_primary_key = 0;" > applicationDB.sql
     tries="1"
-    ${mysql_dump} --lock-tables=false  --no-tablespaces -y  --port=${DB_PORT} --host=${HOST} -u ${DB_U} -p${DB_P} ${DB_N} >> applicationDB.sql
+    ${mysql_dump}  --single-transaction --skip-lock-tables -y --port=${DB_PORT} --host=${HOST} -u ${DB_U} -p${DB_P} ${DB_N} >> applicationDB.sql
     while ( [ "$?" != "0"  ] && [ "${tries}" -lt "5" ] )
     do
         /bin/sleep 10
         tries="`/usr/bin/expr ${tries} + 1`"
-        ${mysql_dump} --lock-tables=false  --no-tablespaces -y  --port=${DB_PORT} --host=${HOST} -u ${DB_U} -p${DB_P} ${DB_N} >> applicationDB.sql
+        ${mysql_dump}  --single-transaction --skip-lock-tables -y --port=${DB_PORT} --host=${HOST} -u ${DB_U} -p${DB_P} ${DB_N} >> applicationDB.sql
     done
     
     if ( [ "${tries}" = "5" ] )
@@ -63,12 +63,12 @@ if ( [ "`${HOME}/providerscripts/utilities/CheckConfigValue.sh DATABASEINSTALLAT
 then
     /bin/echo "SET SESSION sql_require_primary_key = 0;" > applicationDB.sql
     tries="1"
-    ${mysql_dump} --lock-tables=false --no-tablespaces -y --port=${DB_PORT} --host=${HOST} -u ${DB_U} -p${DB_P} ${DB_N} >> applicationDB.sql
+    ${mysql_dump} --single-transaction --skip-lock-tables -y --port=${DB_PORT} --host=${HOST} -u ${DB_U} -p${DB_P} ${DB_N} >> applicationDB.sql
     while ( [ "$?" != "0"  ] && [ "${tries}" -lt "5" ] )
     do
         /bin/sleep 10
         tries="`/usr/bin/expr ${tries} + 1`"
-        ${mysql_dump} --lock-tables=false --no-tablespaces -y --port=${DB_PORT} --host=${HOST} -u ${DB_U} -p${DB_P} ${DB_N} >> applicationDB.sql
+        ${mysql_dump} --single-transaction --skip-lock-tables -y --port=${DB_PORT} --host=${HOST} -u ${DB_U} -p${DB_P} ${DB_N} >> applicationDB.sql
     done
     
     if ( [ "${tries}" = "5" ] )
