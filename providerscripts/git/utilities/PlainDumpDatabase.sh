90a4b4fc6a73ed4c1fad886b6eb40be957d7567d
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-12-03 16:18:59 +0000

    Update PlainDumpDatabase.sh

--- providerscripts/git/utilities/PlainDumpDatabase.sh
+++ providerscripts/git/utilities/PlainDumpDatabase.sh
@@ -58,7 +58,7 @@ then
     fi
    # /bin/sed -i '/SESSION.SQL_LOG_BIN/d' applicationDB.sql
     /bin/echo "DROP TABLE IF EXISTS \`zzzz\`;" >> applicationDB.sql
-    /bin/echo "CREATE TABLE \`zzzz\` ( \`idxx\` int(10) unsigned NOT NULL, PRIMARY KEY (\`idxx\`) ) Engine=INNODB CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;" >> applicationDB.sql
+    /bin/echo "CREATE TABLE \`zzzz\` ( \`idxx\` int(10) unsigned NOT NULL, PRIMARY KEY (\`idxx\`) ) Engine=INNODB CHARSET=utf8mb4;" >> applicationDB.sql
 fi
 
 if ( [ "`${HOME}/providerscripts/utilities/CheckConfigValue.sh DATABASEINSTALLATIONTYPE:MySQL`" = "1" ] || [ "`${HOME}/providerscripts/utilities/CheckConfigValue.sh DATABASEDBaaSINSTALLATIONTYPE:MySQL`" = "1" ] )
@@ -81,7 +81,7 @@ then
     fi
  #   /bin/sed -i '/SESSION.SQL_LOG_BIN/d' applicationDB.sql
     /bin/echo "DROP TABLE IF EXISTS \`zzzz\`;" >> applicationDB.sql
-    /bin/echo "CREATE TABLE \`zzzz\` ( \`idxx\` int(10) unsigned NOT NULL, PRIMARY KEY (\`idxx\`) ) Engine=INNODB CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;" >> applicationDB.sql
+    /bin/echo "CREATE TABLE \`zzzz\` ( \`idxx\` int(10) unsigned NOT NULL, PRIMARY KEY (\`idxx\`) ) Engine=INNODB CHARSET=utf8mb4;" >> applicationDB.sql
 fi
 
 #The Postgres SQL database
