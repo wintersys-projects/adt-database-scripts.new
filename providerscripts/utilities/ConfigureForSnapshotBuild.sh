a64bbb6e39807e6a0e1abf78b4ff22491160188d
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-12-09 12:15:24 +0000

    Update ConfigureForSnapshotBuild.sh

--- providerscripts/utilities/ConfigureForSnapshotBuild.sh
+++ providerscripts/utilities/ConfigureForSnapshotBuild.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 db_prefix="`${HOME}/providerscripts/datastore/configwrapper/ListFromConfigDatastore.sh DBPREFIX:* | /usr/bin/awk -F':' '{print $NF}'`"
-
 command=""
 
 for table in `${HOME}/providerscripts/utilities/helperscripts/ConnectToLocalMySQL.sh 'show tables LIKE "'${db_prefix}'%";' 'raw'`
@@ -12,3 +11,5 @@ done
 command="${command} drop table zzzz;"
 
 ${HOME}/providerscripts/utilities/helperscripts/ConnectToLocalMySQL.sh "${command}"
+
+${HOME}/applicationdb/InstallApplicationDB.sh
