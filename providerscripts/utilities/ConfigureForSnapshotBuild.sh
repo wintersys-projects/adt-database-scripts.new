385c80209d826a7d3fd8217a6ef5bd1e11f657f8
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-12-09 15:17:35 +0000

    Update ConfigureForSnapshotBuild.sh

--- providerscripts/utilities/ConfigureForSnapshotBuild.sh
+++ providerscripts/utilities/ConfigureForSnapshotBuild.sh
@@ -1,20 +1,18 @@
 #!/bin/sh
 
-if ( [ -f ${HOME}/runtime/DATABASE_INSTALLED ] )
+if ( [ -f ${HOME}/runtime/DATABASE_READY ] && [ ! -f ${HOME}/runtime/SNAPSHOT_PRIMED ] )
 then
-        exit
+        db_prefix="`${HOME}/providerscripts/datastore/configwrapper/ListFromConfigDatastore.sh DBPREFIX:* | /usr/bin/awk -F':' '{print $NF}'`"
+        command=""
+
+        for table in `${HOME}/providerscripts/utilities/helperscripts/ConnectToLocalMySQL.sh 'show tables LIKE "'${db_prefix}'%";' 'raw'`
+        do
+                command="${command} drop table ${table};"
+        done
+
+        command="${command} drop table zzzz;"
+
+        ${HOME}/providerscripts/utilities/helperscripts/ConnectToLocalMySQL.sh "${command}"
+
+        ${HOME}/applicationdb/InstallApplicationDB.sh
 fi
-
-db_prefix="`${HOME}/providerscripts/datastore/configwrapper/ListFromConfigDatastore.sh DBPREFIX:* | /usr/bin/awk -F':' '{print $NF}'`"
-command=""
-
-for table in `${HOME}/providerscripts/utilities/helperscripts/ConnectToLocalMySQL.sh 'show tables LIKE "'${db_prefix}'%";' 'raw'`
-do
-        command="${command} drop table ${table};"
-done
-
-command="${command} drop table zzzz;"
-
-${HOME}/providerscripts/utilities/helperscripts/ConnectToLocalMySQL.sh "${command}"
-
-${HOME}/applicationdb/InstallApplicationDB.sh
