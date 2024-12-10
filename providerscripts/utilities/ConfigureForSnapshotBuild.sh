79bfc853dab32214a2f7eafc75d80233d5a21256
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-12-10 17:54:31 +0000

    Update ConfigureForSnapshotBuild.sh

--- providerscripts/utilities/ConfigureForSnapshotBuild.sh
+++ providerscripts/utilities/ConfigureForSnapshotBuild.sh
@@ -2,6 +2,10 @@
 
 if ( [ -f ${HOME}/runtime/DATABASE_READY ] && [ ! -f ${HOME}/runtime/SNAPSHOT_PRIMED ] )
 then
+        if ( [ -f ${HOME}/runtime/CREDENTIALS_PRIMED ] )
+        then
+                /bin/rm ${HOME}/runtime/CREDENTIALS_PRIMED
+        fi
         if ( [ "`${HOME}/providerscripts/utilities/CheckConfigValue.sh DATABASEINSTALLATIONTYPE:MySQL`" = "1" ] || [ "`${HOME}/providerscripts/utilities/CheckConfigValue.sh DATABASEDBaaSINSTALLATIONTYPE:MySQL`" = "1" ] )
         then
                 db_prefix="`${HOME}/providerscripts/datastore/configwrapper/ListFromConfigDatastore.sh DBPREFIX:* | /usr/bin/awk -F':' '{print $NF}'`"
