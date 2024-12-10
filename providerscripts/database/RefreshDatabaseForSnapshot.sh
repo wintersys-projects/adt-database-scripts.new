f53fb56c553102c121c98bbc21cc154afd956bac
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-12-10 20:57:08 +0000

    Update RefreshDatabaseForSnapshot.sh

--- providerscripts/database/RefreshDatabaseForSnapshot.sh
+++ providerscripts/database/RefreshDatabaseForSnapshot.sh
@@ -1,23 +1,32 @@
-        if ( [ -f ${HOME}/runtime/CREDENTIALS_PRIMED ] )
-        then
-                /bin/rm ${HOME}/runtime/CREDENTIALS_PRIMED
-        fi
 
- 	${HOME}/providerscripts/utilities/UpdateInfrastructure.sh
+if ( [ ! -f ${HOME}/runtime/SNAPSHOT_BUILT ] || [ -f ${HOME}/runtime/DATABASE_UPDATED_FOR_SNAPSHOT ] )
+then
+        exit
+fi
 
-        ${HOME}/providerscripts/datastore/configwrapper/GetFromConfigDatastore.sh dbp.dat ${HOME}/runtime/dbp.dat
-        db_prefix="`/bin/cat ${HOME}/runtime/dbp.dat`"
-        if ( [ "`${HOME}/providerscripts/utilities/CheckConfigValue.sh DATABASEINSTALLATIONTYPE:MySQL`" = "1" ] || [ "`${HOME}/providerscripts/utilities/CheckConfigValue.sh DATABASEDBaaSINSTALLATIONTYPE:MySQL`" = "1" ] )
-        then
-                command=""
+${HOME}/providerscripts/utilities/UpdateInfrastructure.sh
+ 
+/bin/touch ${HOME}/runtime/APPLICATION_UPDATED_FOR_SNAPSHOT
+        
+if ( [ -f ${HOME}/runtime/CREDENTIALS_PRIMED ] )
+then
+        /bin/rm ${HOME}/runtime/CREDENTIALS_PRIMED
+fi
 
-                for table in `${HOME}/providerscripts/utilities/helperscripts/ConnectToLocalMySQL.sh 'show tables LIKE "'${db_prefix}'%";' 'raw'`
-                do
-                        command="${command} drop table ${table};"
-                done
+${HOME}/providerscripts/utilities/UpdateInfrastructure.sh
 
-                command="${command} drop table zzzz;"
-                ${HOME}/providerscripts/utilities/helperscripts/ConnectToLocalMySQL.sh "${command}"
-                ${HOME}/applicationdb/InstallApplicationDB.sh
-        fi
-        /bin/touch ${HOME}/runtime/SNAPSHOT_PRIMED
+${HOME}/providerscripts/datastore/configwrapper/GetFromConfigDatastore.sh dbp.dat ${HOME}/runtime/dbp.dat
+db_prefix="`/bin/cat ${HOME}/runtime/dbp.dat`"
+
+if ( [ "`${HOME}/providerscripts/utilities/CheckConfigValue.sh DATABASEINSTALLATIONTYPE:MySQL`" = "1" ] || [ "`${HOME}/providerscripts/utilities/CheckConfigValue.sh DATABASEDBaaSINSTALLATIONTYPE:MySQL`" = "1" ] )
+then
+        command=""
+
+        for table in `${HOME}/providerscripts/utilities/helperscripts/ConnectToLocalMySQL.sh 'show tables LIKE "'${db_prefix}'%";' 'raw'`
+        do
+                command="${command} drop table ${table};"
+        done
+        command="${command} drop table zzzz;"
+        ${HOME}/providerscripts/utilities/helperscripts/ConnectToLocalMySQL.sh "${command}"
+        ${HOME}/applicationdb/InstallApplicationDB.sh
+fi
